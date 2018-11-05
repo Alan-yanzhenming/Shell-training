b1d76fef02f90087eaf750f7e7dc10f2308ac3eb
Alan_yan <1792194483@qq.com>
2018-11-05 23:36:51 +0800

    add training100 and que5.sh

---
+++ training100/que5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+find /123/ -type f -name "*.txt" > /tmp/txt.list
+for f in 'cat /tmp/txt.list';
+do
+   mv $f $f.bak
+done
+
+d=`date +%y%m%d%H%M%S`
+
+mkdir /tmp/123_$d
+
+for f in `cat /tmp/txt.list`;
+do
+   cp $f.bak /tmp/123_$d
+done
+
+cd /tmp
+tar zcf 123.tar.gz 123_$d/
+for f in `cat /tmp/txt.list`;
+do
+  mv $f.bak $find
+done
+
